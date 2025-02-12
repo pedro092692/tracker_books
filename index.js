2acdc94ba4f9c5c2cef1d423d7803345872df94b
pedro <pedro092692@gmail.com>
2025-02-12 17:03:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// ------- PROJECT REQUIREMENTS  ------- \\
+
+// Make tracks books app for review your favorites books and add notes 
+// 1. Add new book for read. (add total pages and paged red show percent)
+// 2. Add score to read book. 
+// 3. Add notes a book ever read books 
+// 4. Sort books by score.
+// 5. Sort books by read and not reads. 
+// 6. New books notes.
+// 7. Using this api to get books info https://openlibrary.org/dev/docs/api/covers
+
