43f7d301cf9219884f03924415488770890af803
pedro <pedro092692@gmail.com>
2025-02-26 09:41:36 +0100

    add styles in search view

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //index view 
 app.get('/', async(req, res) => {
+    error = undefined;
     const userBooks = await librarayBooks();
 
     const suggesBooks = await randomBook();
